4e09d81b32c62b8f38c0936a4ebe027709e54ccc
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-08-30 19:20:36 -0500

    comit variables y operaciones python

--- clase_uno.py
+++ clase_uno.py
@@ -5,3 +5,71 @@ Created on Mon Aug 30 18:54:54 2021
 @author: Desarrollo
 """
 
+print("hola mundo");
+
+# variables
+a = 3
+print(type(a))
+
+a = "hola"
+print(type(a))
+
+a = 3.5
+print(type(a))
+
+# suma
+
+a = 5
+b = 2
+c = a + b
+print(c)
+
+# resta
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# multiplicacion
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# division
+
+a = 5
+b = 2
+c = a / b
+print(c)
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# potencia
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# raiz
+
+a = 5
+b = 2
+c = a  ** (1/3)
+print(c)
+
+import math
+raiz = math.sqrt(25)
+print(raiz)
+
+
+
+
+
+
