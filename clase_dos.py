7b3cbc720a2ce9f83ebde521d804f87ab4ef903d
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-06 19:25:27 -0500

    commit clase dos tabla de verdad y jerarquias

--- clase_dos.py
+++ clase_dos.py
@@ -4,4 +4,34 @@ Created on Mon Sep  6 19:04:01 2021
 
 @author: Desarrollo
 """
+# condicionales
 
+# Tabla de Verdad
+
+# Tabla del AND
+# V and V = V
+# V and F = F
+# F and V = F
+# F and F = F
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+
+# Tabla del OR
+# V or V = V
+# V or F = V
+# F or V = V
+# F or F = F
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+
+# Tabla de Negacion
+# not V = F
+# Vnot F = V
+print(not True)  # False
+print(not False)  # True
