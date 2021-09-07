9fe19e3f109b52ee5c4783af7e50e5904f173363
Desarrollo <Desarrollo@DESKTOP-F722E5D>
2021-09-06 19:06:01 -0500

    commit clase dos inicial

---
+++ clase_dos.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 19:04:01 2021
+
+@author: Desarrollo
+"""
+
--- clase_uno.py
+++ clase_uno.py
@@ -158,6 +158,55 @@ valor_total = varlor_producto - valor_descuento
 print(f"Valor producto: ${varlor_producto:,}\n Valor descuento: ${valor_descuento:,}\n Valor total: ${valor_total:,}")
 
 
+# ------Calcule el valor de Y indicando paso a paso como llegó al resultado 1.1
+# 1.1    y = ( (5+2-5)^2 * 5+8/2 -30 ) / 2 * 5 -3
+# 1.1    y = ( (2)^2 * 5+ 4 -30 ) / 2 * 5 -3
+# 1.1    y = ( 4 * 5 + 4 - 30 ) / 2 * 5 -3
+# 1.1    y = (20 + 4 -30 ) / 2 * 5 -3
+# 1.1    y = (-6) / 2 * 5 -3
+# 1.1    y = -3 * 5 -3
+# 1.1    y = -18
+
+ej1_1resultado = ((((5 + 2 - 5) ** 2) * 5 + (8 / 2) - 30) / 2) * 5 - 3
+print(f"ej 1.1 y = ((((5+2-5)**2) * 5 + (8/2) -30 ) / 2 )* 5 -3")
+print(f"ej 1.1 Resultado: {ej1_1resultado}")
+
+
+
+# ------Calcule el valor de Y indicando paso a paso como llegó al resultado 1.2
+
+# z=5
+# n=3
+# m= z-n
+# y = (( (z+2-n)^2 * m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3 
+
+ej1_2z = 5
+ej1_2n = 3
+ej1_2m = ej1_2z - ej1_2n
+ej1_2resultado = float((((((ej1_2z + 2 - ej1_2n)** 2) * ej1_2m + 8 / 2 - 30) / 2) * 5 - 3)** 5 + 15 * 3 - (9 / 3))
+print("ej 1.2 y = ((((5+2-5)**2) * 5 + (8/2) -30 ) / 2 )* 5 -3")
+print(f"ej 1.2 Resultado: {ej1_2resultado}")
+
+# ------Calcule el valor de Y indicando paso a paso como llegó al resultado 1.3
+# z=5
+# n=( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3
+# m= z^2*3+n
+# m= (5)^2*3+(( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3)
+# y = ((( (z+2-n)^2 x m+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3) ^ 2 – 5/4
+# y = ((( (5+2-(( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3))^2 * ((5)^2*3+(( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3))+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3) ^ 2 – 5/4
+
+ej1_3z = float(5)
+ej1_3n = float((((8 + 2 - 4)**2) * 5 + 8 + 7 / 2 - 30 * 5)/ 2 * 5 - 3)
+ej1_3m = float(((ej1_3z)**2) * 3 + (ej1_3n))
+print(f"ej 1.2 Resultado: {ej1_3m}  ,{ej1_3n} , {ej1_3z} ")
+
+
+# ej1_3resultado = float( (((((ej1_3z) + 2 - ((ej1_3n)**2) * ej1_3m + 8/2 - 30 ) / 2 * 5 - 3)** 5 + 15 * 3 - 9/3)**2 – 5/4)
+
+ej1_3resultado = float(((((((((ej1_3z)+2-(ej1_3n)) ** 2) * (ej1_3m)+8/2 -30 ) / 2 * 5 -3) ** 5) + 15 * 3 - 9/3) ** 2) - (5/4))
+
+print("ej 1.3 y = y = ((( (5+2-(( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3))^2 * ((5)^2*3+(( (8+2-4)^2 * 5+8+7/2 -30*5 ) / 2 * 5 -3))+8/2 -30 ) / 2 * 5 -3)^ 5 + 15 * 3 - 9/3) ^ 2 – 5/4")
+print(f"ej 1.3 Resultado: {ej1_3resultado}")
 
 
 
